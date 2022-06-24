dcb72025bcfec2348f91de4317f59416b453ba77
Anastasia <anastasiavlg@mail.ru>
2022-06-25 00:35:25 +0300

    Add short format to display information about numbers

---
+++ Amazing Numbers/task/src/numbers/ShortFormat.java
@@ -0,0 +1,16 @@
+package numbers;
+
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class ShortFormat implements OutputFormat {
+    @Override
+    public void display(String number) {
+        System.out.printf("%s is %s\n", number,
+                Stream.of(Property.ALL)
+                        .filter(p -> p.getChecker().test(number))
+                        .map(Property::toString)
+                        .collect(Collectors.joining(", "))
+                        .toLowerCase());
+    }
+}
